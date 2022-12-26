e7c187e80245ec02a5384e649ebf0cc4a506bd38
Shruthisayaa <121433305+Shruthisayaa@users.noreply.github.com>
2022-12-26 18:24:31 +0530

    Add files via upload
    Week1.day1

---
+++ day1/Browser1.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Browser1 {
+	
+	
+	public static void main(String[] args)
+	{
+		Chrome s1= new Chrome();
+		s1.getName();
+		s1.printName();
+}
+}
---
+++ day1/Student.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Student {
+	public void variables()
+	{
+		String StudentName ="Shruthi";
+		System.out.println("Enter the Student Name :\n" +StudentName);
+		int RollNo=1;
+		System.out.println("Enter the student roll no:\n"+RollNo);
+		String CollegeName="SRM";
+		System.out.println("Enter the college Name:\n"+CollegeName);
+		int MarkScored=799;
+		System.out.println("Enter the Mark scored:\n"+MarkScored);
+		float Cgpa=6.490f;
+		System.out.println("Enter the student's CGPA:\n"+Cgpa);
+		
+	}
+
+}
